f258cee125280633c23d9ec85781af6ea54a2f9e
Анастасия Казарина <ana-kazarina@MacBook-Pro-Anastasia.local>
2022-12-25 15:21:52 +0300

    Написала программу к задаче 4

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int namber = int.Parse(Console.ReadLine()!);
+
+if(namber % 2 == 0){
+    Console.WriteLine("ДА. Число четное");
+}
+else{
+    Console.WriteLine("НЕТ. Число не четное");
+}
